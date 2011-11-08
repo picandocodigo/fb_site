5247ee1c69f67d0880973bce2ba3b6407215fd83
Fernando Briano <fernando@picandocodigo.net>
2011-11-08 14:38:25 -0200

    Added pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def wordpress
+  end
+
+  def github
+  end
+
+  def freesoftware
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 FbSite::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/wordpress"
+
+  get "pages/github"
+
+  get "pages/freesoftware"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +58,7 @@ FbSite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
