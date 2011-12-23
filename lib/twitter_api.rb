d20b92117d0bd58a02fc0487011912b632e83dfd
Fernando Briano <fernando@picandocodigo.net>
2011-12-23 03:27:52 -0200

    Parsed links on tweet for tags, screen names and links

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,6 +1,7 @@
 require 'httparty'
 require 'twitter_user'
 require 'error_display'
+require 'rails'
 
 module TwitterApi
 
@@ -22,8 +23,42 @@ module TwitterApi
       date = DateTime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S')
       #TODO: Get date format from a yaml file 
       tweet['created_at'] = date.strftime("%d/%m/%Y %H:%M:%S")
+      #"user_mentions"=>[{"id_str"=>"16634447", "name"=>"xombra", "indices"=>[3, 10], "screen_name"=>"xombra", "id"=>16634447},
+      # ], "user_mentions"=>[{"id_str"=>"18435696", "name"=>"Daniel M. Maldonado", "indices"=>[3, 13], "screen_name"=>"elcodigok", "id"=>18435696}]},
+      text = tweet['text'] 
+      if (!tweet['entities']['user_mentions'].nil?)
+        tweet['entities']['user_mentions'].each do |mention|
+          link = "<a href='http://twitter.com/#{mention['screen_name']}'>@#{mention['screen_name']}</a>"
+          #tweet['text'] = text.gsub("@#{mention['screen_name']}", link)
+          #string = tweet['text'][mention['indices'][0], mention['indices'][1]]
+          string = "@#{mention['screen_name']}"
+          #tweet['text'].gsub!(string, link)
+          set_links tweet['text'], string, link
+        end
+      end
+      #"hashtags"=>[{"indices"=>[15, 20], "text"=>"Ruby"}, {"indices"=>[31, 39], "text"=>"cerveza"}],
+      if (!tweet['entities']['hashtags'].nil?)
+        tweet['entities']['hashtags'].each do |tag|
+          string = "\##{tag["text"]}"
+          link = "<a href='http://twitter.com/#!/search?q=%23#{tag['text']}'>\##{tag['text']}</a>"
+          set_links tweet['text'], string, link
+        end
+      end
+       
+      #"urls"=>[{"url"=>"http://t.co/wyG7MRgj", "indices"=>[88, 108], "expanded_url"=>"http://xkcd.com/323/", "display_url"=>"xkcd.com/323/"}],
+      if (!tweet['entities']['urls'].nil?)
+        tweet['entities']['urls'].each do |url|
+          string = url['url']
+          link = "<a href='#{url['expanded_url']}'>#{url['url']}</a>"
+          set_links tweet['text'], string, link
+        end
+      end 
     end
     tweets
+  end
+  
+  def self.set_links text, string, link
+    text.gsub!(string, link)
   end
   
 end
