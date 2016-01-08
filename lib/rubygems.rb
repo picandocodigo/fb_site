1ef3dbb6ec653d4305f46a9f65a954a0301f3360
Fernando Briano <fernando@picandocodigo.net>
2016-01-08 19:49:12 -0300

    Use URI parse read instead of net/http)

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -1,10 +1,10 @@
-require 'net/http'
 require 'open-uri'
 require 'json'
 
 module RubyGems
   def self.info(user)
-    JSON.parse(Net::HTTP.get('rubygems.org',"/api/v1/owners/#{user}/gems.json"))
+    JSON.parse(
+      URI.parse("https://rubygems.org/api/v1/owners/#{user}/gems.json").read
+    )
   end
-
 end
