35e3bb2ba3f5021a1f2305f94de1cac552bb4190
Fernando Briano <fernando@picandocodigo.net>
2017-08-28 19:42:10 +0100

    Cleanup

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -5,6 +5,6 @@ module RubyGems
   def self.info(user)
     JSON.parse(
       URI.parse("https://rubygems.org/api/v1/owners/#{user}/gems.json").read
-    )
+    ).sort_by { |gem| gem['downloads'].to_i }.reverse
   end
 end
