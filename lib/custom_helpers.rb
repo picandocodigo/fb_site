c272f981d57c9f45699f050a6fa3749ad74bf1c7
Fernando Briano <fernando@picandocodigo.net>
2019-05-11 00:06:18 +0100

    Format download numbers

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -35,6 +35,10 @@ module CustomHelpers
       .join(', ')
   end
 
+  def format_number(number)
+    number.to_s.chars.to_a.reverse.each_slice(3).map(&:join).join(',').reverse
+  end
+
   private
 
   def env
